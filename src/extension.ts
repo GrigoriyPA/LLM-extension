638be6c5baae0bf54f60122974d565a01177333f
Григорий Писаренко <ga_pisarenko@mail.ru>
2024-01-25 00:13:06 +0300

    Wrote simple python extension

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,40 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+import { exec } from 'child_process';
+import { text } from 'stream/consumers';
+
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export async function activate(context: vscode.ExtensionContext) {
+	vscode.languages.registerCompletionItemProvider('python', {
+		provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
+			const complitionItem = new vscode.CompletionItem(
+				'MyCompletionItem',
+				vscode.CompletionItemKind.User
+			);
+			return [complitionItem];
+		}
+	});
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "llm-extension" is now active!');
+	vscode.languages.registerHoverProvider('python', {
+		async provideHover(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken) {
+			const range = document.getWordRangeAtPosition(position);
+			const word = document.getText(range);
+			const { stdout } = exec(`python -c "print(${word}.__doc__)"`);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
+			if (stdout === null) {
+				throw new Error('Could not fetch hover data');
+			}
+
+			return {
+				contents: [await text(stdout)]
+			};
+		}
+	});
+
 	let disposable = vscode.commands.registerCommand('llm-extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from LLM-extension!');
 	});
 
 	context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
