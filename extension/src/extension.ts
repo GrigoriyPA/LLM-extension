a155d10b5031115150167cb4342e22819b9ac718
Григорий Писаренко <ga_pisarenko@mail.ru>
2024-01-27 22:04:42 +0300

    Added insert example

--- extension/src/extension.ts
+++ extension/src/extension.ts
@@ -3,9 +3,15 @@ import net from 'node:net';
 
 import * as vscode from 'vscode';
 import {
+    HoverRequest,
+    Hover,
+    Position,
+    TextDocumentIdentifier,
     LanguageClient,
     LanguageClientOptions,
-    StreamInfo
+    StreamInfo,
+    HoverParams,
+    MarkupContent
 } from 'vscode-languageclient/node';
 
 let client: LanguageClient;
@@ -71,10 +77,26 @@ export async function activate(context: vscode.ExtensionContext) {
         printToExtentionChannel(`Failed to connect language client to Jedi LS:\n${e}`);
     }
 
-    let disposable = vscode.commands.registerTextEditorCommand('llm-extension.documentFunction', (textEditor: vscode.TextEditor, edit: vscode.TextEditorEdit) => {
-        const position = textEditor.selection.active;
+    let disposable = vscode.commands.registerTextEditorCommand('llm-extension.documentFunction', async (textEditor: vscode.TextEditor, edit: vscode.TextEditorEdit) => {
+        const position: vscode.Position = textEditor.selection.active;
         const range = textEditor.document.getWordRangeAtPosition(position);
         const world = textEditor.document.getText(range);
+        
+        let params: HoverParams = {
+            textDocument: TextDocumentIdentifier.create(textEditor.document.uri.toString()),
+            position: Position.create(position.line, position.character)
+        };
+
+        const response: Hover = await client.sendRequest(HoverRequest.method, params);
+        if (response !== null) {
+            const content = response.contents as MarkupContent;
+            printToExtentionChannel(`Hover content:\n${content.value}`);
+        }
+
+        textEditor.edit((edit: vscode.TextEditorEdit) => {
+            edit.insert(position, "Test insert content");
+        });
+
         vscode.window.showInformationMessage(`Hello World from LLM-extension! Current world: ${world}`);
     });
 
