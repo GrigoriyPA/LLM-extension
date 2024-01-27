7c48f59048b93440bcd2f562cd6be3bd1b7ac22e
zenman123 <artemsafin67@yandex.ru>
2024-01-27 19:30:55 +0000

    wrote class for llms

---
+++ ml/text_data/prompts.py
@@ -0,0 +1,34 @@
+DOCSTRING_PROMPT = """Generate a docstring by the given declaration, 
+definition (optional) and context (optional) of the function. 
+Docstring must include detailed and extensive description of the function,
+it's parameters and return value. 
+Docstring must follow accurate format:
+{a few sentences describing what function do}
+{description of each argument}
+{description of a return value}.
+Here is the body of the function, 
+for which you will have to write a docstring: """
+DOCSTRING_PROMPT = DOCSTRING_PROMPT.replace("\n", "")
+
+
+"""
+
+def find_maximum(arr):
+    max_el = -1
+    for el in arr:
+        max_el = max(max_el, el)
+    return max_el"
+->
+Function find_maximum finds the biggest element in the list 
+
+Parameters
+----------
+arr: list[int]
+    List of numbers
+
+Returns
+-------
+int
+    The maximum element from the array
+    
+"""
