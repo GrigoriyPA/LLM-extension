44e5f450e1c677fe4716300dbff7dbc78bd2050f
Timur Nasretdinov <timurka_nasretdinov@mail.ru>
2024-03-24 19:06:59 +0300

    Added HTTP-responses to server

--- extension/src/utils/http_server/requests_functions.ts
+++ extension/src/utils/http_server/requests_functions.ts
@@ -2,6 +2,8 @@ import { RequestsBase, ResponseBase } from "./requests_structures";
 
 import { LogLevel, Components, logEntry } from "../logger";
 
+import fetch from "node-fetch";
+
 function logMessage(logLevel: LogLevel, functionName: string, message: string) {
     logEntry(logLevel, Components.HTTP_GATEWAY, `[${functionName}] ${message}`);
 }
@@ -18,8 +20,14 @@ export async function sendRequest(
 
     // TODO: @dffTu implement request sending and error message passing
     // TODO: @ganvas show status bar when we wait for http response
+    const response_raw = await fetch('http://dfftu.pythonanywhere.com', {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: request.serialize()
+    });
+
     const response = new ResponseBase.HttpResponse(
-        `Test response for request ${request.getName()}`
+        await response_raw.text()
     );
     response.setSuccess();
 
--- server/server.py
+++ server/server.py
@@ -1,19 +1,73 @@
 from flask import Flask, request
+from server_config import REQUIRED_FIELDS
+import json
 
 app = Flask(__name__)
 
 
-def get_docstring(some_args=None):
-    print("Getting docstring...")
-    return 0
+def document_function(some_args=None):
+    print("Getting document of function...")
+    return "Some document"
 
 
-@app.route("/", methods=['POST'])
+def semantic_analysis_of_symbol(some_args=None):
+    print("Getting semantic_analysis_of_symbol...")
+    return "Some analysis"
+
+
+def name_suggestion(some_args=None):
+    print("Getting name suggestion...")
+    return "Some name suggestion"
+
+
+def generate_tests(some_args=None):
+    print("Generating tests for function...")
+    return "Some tests"
+
+
+def completion_suggestion(some_args=None):
+    print("Getting completion suggestions...")
+    return ["Suggestion 1", "Suggestion 2"]
+
+
+@app.route("/", methods=['GET', 'POST'])
 def handle_request():
-    args = [(el, request.form[el]) for el in request.form.keys()]
-    if "type_of_request" in request.form and request.form["type_of_request"] == "get_docstring":
-        get_docstring()
-    return "POST ARGUMENTS: " + str(args)
+    if request.method == 'GET':
+        return 'SOME GET REQUEST'
+    response = {
+        "error_message": "",
+        "single_string": "",
+        "multiple_strings": []
+    }
+    data = request.json
+    for field in REQUIRED_FIELDS:
+        if field not in data:
+            response["error_message"] = f"No '{field}' field in request."
+            return json.dumps(response)
+
+    request_type = data["type_of_request"]
+    symbol_content = data["symbol_content"]
+    references_content = data["references_content"]
+
+    print(request_type)
+    try:
+        match request_type:
+            case "DocumentFunction":
+                response["single_string"] = document_function(symbol_content)
+            case "SemanticAnalysisOfSymbol":
+                response["single_string"] = semantic_analysis_of_symbol(symbol_content)
+            case "NameSuggestion":
+                response["single_string"] = name_suggestion(symbol_content)
+            case "GenerateTests":
+                response["single_string"] = generate_tests(symbol_content)
+            case "CompletionSuggestion":
+                response["multiple_strings"] = completion_suggestion(symbol_content)
+            case _:
+                response["error_message"] = f"Unknown request type: {request_type}."
+    except Exception as error:
+        response["error_message"] = str(error)
+    return json.dumps(response)
 
 
-app.run()
+app.run(port=8081)
+
