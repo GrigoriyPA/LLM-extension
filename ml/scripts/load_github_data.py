47cb69dbbd3e08f6af7b89f0fa83774a25bf071a
Sorokin Dmitriy <rik@Sorokins-MacBook-Pro.local>
2024-04-01 14:12:56 +0300

    create script flow

--- ml/scripts/load_github_data.py
+++ ml/scripts/load_github_data.py
@@ -3,9 +3,14 @@ import pandas as pd
 from src import github_searcher
 from tqdm import tqdm
 
+# token generation: https://github.com/settings/tokens
 
-with open('config.json') as f:
+with open('scripts/config.json') as f:
     config = json.load(f)
+
+if config['github_token'] == 'your_token':
+    print('replace github token')
+    exit()
 
 
 for repo in tqdm(config['repos']):
@@ -22,5 +27,4 @@ for repo in tqdm(config['repos']):
         row = data[func_name]
         pd_data.loc[-1] = (row['name'], row['docstring'], row['code'], row['usages'])
         pd_data.index = pd_data.index + 1
-    pd_data.to_csv(f'{author}_{repo_name}.csv')
-
+    pd_data.to_csv(f'{config["data_path"]}/{author}_{repo_name}.csv')
